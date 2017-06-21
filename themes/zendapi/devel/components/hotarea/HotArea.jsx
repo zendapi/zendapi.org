a6b2da1782333d3580545695c08ea71492326a21
SOFTBOY <zzu_softboy@163.com>
2017-06-22 00:30:17 +0800

    首页按钮完成

--- themes/zendapi/devel/components/hotarea/HotArea.jsx
+++ themes/zendapi/devel/components/hotarea/HotArea.jsx
@@ -7,6 +7,10 @@ export default class HotArea extends React.Component
       return <div className="uk-container uk-container-expand hot-area uk-text-center">
          <div className="zendapi">zendAPI</div>
          <div className="slogan">Redefined The API of Zend Engine</div>
+         <div className="btn uk-margin-small">
+            <button className="uk-button uk-button-default uk-margin-medium-right uk-box-shadow-hover-large">开始学习</button>
+            <button className="uk-button uk-button-default">GitHub仓库</button>
+         </div>
       </div>;
    }
 };
--- themes/zendapi/devel/layout/index.jsx
+++ themes/zendapi/devel/layout/index.jsx
@@ -1,16 +1,29 @@
-"use strict";
-
+  "use strict";
++
 require("uikit");
 import React from "react";
 import ReactDOM from "react-dom";
 import Header from "Components/header/Header";
 import HotArea from "Components/hotarea/HotArea";
 import Footer from "Components/footer/Footer";
-import Index from "Js/pages/Index";
+import Index from "Js/pages/index";
+
+class IndexPage extends React.Component
+{
+   render()
+   {
+       return <div style={{width:"100%"}} className="uk-box-shadow-large">
+           <HotArea/>
+       </div>;
+   }
+}
 
 
 $(function ()
 {
    ReactDOM.render(<Header/>, document.getElementById("header-wrapper"));
-   ReactDOM.render(<HotArea/>, document.getElementById("container"));
+   ReactDOM.render(<IndexPage/>, document.getElementById("container"));
 });
+
+
+
