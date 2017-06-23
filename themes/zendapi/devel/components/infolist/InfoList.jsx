f7f4cd821c0fc9a05005fa0f8a192450bb36fd9c
softboy <zzu_softboy@163.com>
2017-06-23 08:20:06 +0800

    完成信息模块

--- themes/zendapi/devel/components/infolist/InfoList.jsx
+++ themes/zendapi/devel/components/infolist/InfoList.jsx
@@ -8,12 +8,12 @@ export default class InfoList extends React.Component
    render()
    {
       return <div className="info-list-panel uk-margin-medium-top uk-flex uk-flex-center">
-         <div className="uk-child-width-expand@s" data-uk-grid>
-            <div className="uk-width-1-2@s">
-               <div className="uk-flex uk-flex-column uk-width-1-1">
-                  <div className="panel-title uk-text-left uk-margin-small-top">
+         <div className="uk-child-width-expand@s grid-wrapper" data-uk-grid>
+            <div className="uk-width-1-1 uk-width-1-2@s info-item">
+               <div className="uk-flex uk-flex-column">
+                  <div className="panel-title uk-text-center uk-text-left@s uk-margin-small-top">
                      <img className="title-icon" src={ManualSvg} />推荐阅读</div>
-                  <ul className="info-list uk-text-left uk-margin-medium-top uk-list uk-list-divider">
+                  <ul className="info-list uk-text-left uk-margin-small-top uk-list uk-list-divider">
                      {this.renderInfoItem("对Zend引擎的一维C接口重新进行组织，按照不同的模块")}
                      {this.renderInfoItem("对Zend引擎的一维C接口重新进行组织，按照不同的模块")}
                      {this.renderInfoItem("对Zend引擎的一维C接口重新进行组织，按照不同的模块,asdasdasdads")}
@@ -22,13 +22,13 @@ export default class InfoList extends React.Component
                   </ul>
                </div>
             </div>
-            <div className="uk-width-1-2@s">
-               <div className="uk-flex uk-flex-column uk-width-1-1">
-                  <div className="panel-title uk-text-left uk-margin-small-top">
+            <div className="uk-width-1-1 uk-width-1-2@s info-item">
+               <div className="uk-flex uk-flex-column">
+                  <div className="panel-title uk-text-center uk-text-left@s uk-margin-small-top">
                      <img className="title-icon" src={NewsSvg} />开发动态</div>
-                  <ul className="info-list uk-text-left uk-margin-medium-top uk-list uk-list-divider">
+                  <ul className="info-list uk-text-left uk-margin-small-top uk-list uk-list-divider">
                      {this.renderInfoItem("对Zend引擎的一维C接口重新进行组织，按照不同的模块")}
-                     {this.renderInfoItem("对Zend引擎的一维C接口重新进行组织，按照不同的模块")}
+                     {this.renderInfoItem("对Zend引擎的一维C接口重新进行组织，按照不同的模块阿斯蒂芬啥都分割后")}
                      {this.renderInfoItem("对Zend引擎的一维C接口重新进行组织，按照不同的模块")}
                      {this.renderInfoItem("对Zend引擎的一维C接口重新进行组织，按照不同的模块")}
                      {this.renderInfoItem("对Zend引擎的一维C接口重新进行组织，按照不同的模块")}
