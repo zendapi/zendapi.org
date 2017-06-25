62c34abc15df4dc8b24835109bb5251415f3b85f
softboy <zzu_softboy@163.com>
2017-06-25 19:45:18 +0800

    修改顶部导航

--- themes/zendapi/devel/components/header/Header.jsx
+++ themes/zendapi/devel/components/header/Header.jsx
@@ -12,11 +12,11 @@ export default class Header extends React.Component
          </div>
          <div className="uk-navbar-right">
             <ul className="uk-navbar-nav uk-visible@m">
-               <li className="uk-active"><a href="#">网站首页</a></li>
+               <li className="uk-active"><a href="/">网站首页</a></li>
                <li><a href="#">手册</a></li>
                <li><a href="#">API文档</a></li>
-               <li><a href="#">博客</a></li>
-               <li><a href="#">关于我们</a></li>
+               <li><a href="/blog">博客</a></li>
+               <li><a href="/about">关于我们</a></li>
             </ul>
             <ul className="uk-navbar-nav uk-hidden@m">
                <li><button className="nav-menu" data-uk-icon="icon: menu" data-uk-toggle="target: #offcanvas-push"></button></li>
