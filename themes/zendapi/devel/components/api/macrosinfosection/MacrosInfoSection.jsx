e05eb7900dcbe6e0e7baf7c97ee1d68ba20d3522
softboy <zzu_softboy@163.com>
2017-07-08 15:26:05 +0800

    宏定义的详细项完成

--- themes/zendapi/devel/components/api/macrosinfosection/MacrosInfoSection.jsx
+++ themes/zendapi/devel/components/api/macrosinfosection/MacrosInfoSection.jsx
@@ -12,7 +12,7 @@ export default class MacrosInfoSection extends React.Component
          <div className="section-title uk-margin-medium-bottom">
             <h3>宏定义详细文档</h3>
          </div>
-         {macros.map((item, index) => 
+         {macros.map((item, index) =>
             <div className="section-item uk-margin-medium-bottom" key = {"macroinfosectionitem"+index}>
                <a id={item.id}/>
                <div className="uk-flex uk-flex-wrap section-item-name">
@@ -21,12 +21,20 @@ export default class MacrosInfoSection extends React.Component
                      <span>{item.name}</span></div>
                </div>
                <div className="uk-background-muted uk-text-break macro-definition definition uk-flex uk-flex-left uk-flex-wrap">
-                  <span className="define-keyword">#define</span> 
+                  <span className="define-keyword">#define</span>
                   <span className="uk-text-break name">
                      {item.name}
                      {item.params && item.params.length > 0 && "( " +item.paramsString + " )"}
                   </span>
                   {this.shouldRenderInitializer(item.initializer) && <div className="define uk-text-primary" dangerouslySetInnerHTML={{__html:item.initializer}}></div>}
+               </div>
+               <div className="uk-text-break">
+                  #include &lt;<a href={item.containerRef.url}>{item.location.file}</a>&gt;
+               </div>
+               {item.briefDescription.length > 0 && <div className="uk-margin-small-top uk-text-break">{item.briefDescription}</div>}
+               {item.detailDescription.length > 0 && <div className="uk-margin-small-top uk-text-break">{item.detailDescription}</div>}
+               <div className="uk-margin-small-top uk-text-break">
+                  在文件 <span className="uk-text-success">{item.location.file.substring(8)}</span> 的第 <span className="uk-text-success">{item.location.line}</span> 行定义
                </div>
             </div>
          )}
