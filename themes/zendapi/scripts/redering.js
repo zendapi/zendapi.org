e317d65794a5c170692f43d5b16d9e8e07248526
softboy <zzu_softboy@163.com>
2017-10-31 20:33:33 +0800

    refactor cpp api generator

--- themes/zendapi/scripts/redering.js
+++ themes/zendapi/scripts/redering.js
@@ -6,20 +6,20 @@ const minify = require('html-minifier').minify;
 const UglifyJS = require('uglify-es');
 var CleanCSS = require('clean-css');
 
-hexo.extend.filter.register('after_render:html', function(str, data){
-    return minify(str, {
-        enable: true,
-        exclude: [],
-        ignoreCustomComments: [/^\s*more/],
-        removeComments: true,
-        removeCommentsFromCDATA: true,
-        collapseWhitespace: true,
-        collapseBooleanAttributes: true,
-        removeEmptyAttributes: true,
-        minifyJS: true,
-        minifyCSS: true
-    });
-});
+// hexo.extend.filter.register('after_render:html', function(str, data){
+//     return minify(str, {
+//         enable: true,
+//         exclude: [],
+//         ignoreCustomComments: [/^\s*more/],
+//         removeComments: true,
+//         removeCommentsFromCDATA: true,
+//         collapseWhitespace: true,
+//         collapseBooleanAttributes: true,
+//         removeEmptyAttributes: true,
+//         minifyJS: true,
+//         minifyCSS: true
+//     });
+// });
 
 hexo.extend.filter.register('after_render:js', function(str, data){
     var result = UglifyJS.minify(str);
